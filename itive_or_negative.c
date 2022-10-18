f495aae522253bad79b0be5d4d957c84cf906e75
Fuproary <aminaabdullahi389@gmail.com>
2022-10-18 08:01:39 -0700

    task8

---
+++ itive_or_negative.c
@@ -0,0 +1,77 @@
+[33mcommit 1aae5ff87e10d7b6eee504fb102c2024a08ad2ef[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Mon Oct 17 08:55:34 2022 -0700
+
+    task zero
+
+[33mcommit b84bacf3c1339e24e4e3c7a101cccc2c13412c85[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Sun Oct 16 01:02:04 2022 -0700
+
+    task 4
+
+[33mcommit b95d52b0ed114e39d733ea9c812f2703730aa832[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Sun Oct 16 00:13:15 2022 -0700
+
+    task 5 updated
+
+[33mcommit 346b73280e135d42bd69e3fb09c004a57d74bf71[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Sat Oct 15 23:39:08 2022 -0700
+
+    updated task six
+
+[33mcommit 6cf410d4a39ac15e28d82a9638816d464578f996[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Sat Oct 15 08:34:19 2022 -0700
+
+    task six
+
+[33mcommit 2f0d4d1bf5a5d753fc6e7a35d24373117cadd7db[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 17:28:36 2022 -0700
+
+    task 5
+
+[33mcommit 098c283be37e63013e742f62bce82234bacead16[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 17:10:10 2022 -0700
+
+     printf task
+
+[33mcommit c1dc0b0935860245968b58a5afa01bdde3d639da[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 15:30:25 2022 -0700
+
+     puts
+
+[33mcommit d8c92b8c832a03aadc7891da0dc13d2161c3dffb[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 12:44:31 2022 -0700
+
+    puts task
+
+[33mcommit 11c8f342eb3f9d9c10f0e24bbeb5224b45995aaf[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 12:02:19 2022 -0700
+
+    name task
+
+[33mcommit d97a07dcf637e16826768945d5fb521e18e2e432[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 11:48:51 2022 -0700
+
+    assembler task
+
+[33mcommit 43a893b31bea33351ea713155d04243728dc0e18[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 11:41:37 2022 -0700
+
+    compiler task
+
+[33mcommit 3937979bbafed1272f8b312ed173ac7f84f46dad[m
+Author: Fuproary <aminaabdullahi389@gmail.com>
+Date:   Fri Oct 14 11:30:19 2022 -0700
+
+     C commit
